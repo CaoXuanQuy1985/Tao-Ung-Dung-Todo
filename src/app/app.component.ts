b9e233ae78f3f10dd0c9588e33658d2aaecc7def
CaoXuanQuy1985 <cun.kin1985.working@gmail.com>
2020-07-06 13:21:08 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl} from '@angular/forms';
+
+interface ITodo {
+  id: number;
+  content: string;
+  complete: boolean;
+}
+
+let _id = 1;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tao-Ung-Dung-Todo';
+  userInput = new FormControl();
+  todos: Array<ITodo> = [];
+  constructor() {}
+
+  ngOnInit() {}
+
+  onChange() {
+    const {value} = this.userInput;
+    if (value) {
+      const todo: ITodo = {
+        id: _id++,
+        content: value,
+        complete: false
+      };
+      this.todos.push(todo);
+      this.userInput.setValue(' ');
+    }
+  }
+
+  toggleTodo(i) {
+    this.todos[i].complete = !this.todos[i].complete;
+  }
 }
